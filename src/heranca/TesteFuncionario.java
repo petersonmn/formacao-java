014b6ff54d9d126bdf8f15c792f64a6cf1847037
Peterson Martins <petymn@hotmail.com>
2021-09-02 11:47:46 -0300

    Introdução a herança

---
+++ src/heranca/TesteFuncionario.java
@@ -0,0 +1,15 @@
+package heranca;
+
+public class TesteFuncionario {
+	public static void main(String[] args) {
+		
+		Funcionario peterson = new Funcionario();
+		peterson.setNome("Peterson Martins");
+		peterson.setCpf("999.999.999-99");
+		peterson.setSalario(2600.00);
+		
+		System.out.println(peterson.getNome());
+		System.out.println(peterson.getBonificacao());
+	}
+
+}
